e788d09d978b6a1e0331ed030ff5a192cd85063a
Luis Montes <luis@iceddev.com>
2014-02-28 15:45:48 -0700

    ability to restart

---
+++ launcher.js
@@ -0,0 +1,56 @@
+var db = require('./lib/level');
+var getToken = require('./lib/getToken');
+var webserver = require('./webserver');
+var sendLanIp = require('./lib/sendLanIp');
+var subdevices = require('./lib/subdevices');
+var getGatewayId = require('./lib/getGatewayId');
+var connectSkynet = require('./lib/connectSkynet');
+
+
+function launch(){
+  var gatewayId;
+  var token;
+  var port = process.env.PORT || 8888;
+  var conn;
+  var subdevicesMessenger;
+
+  function updateIp(){
+    sendLanIp(gatewayId, token, port, conn);
+    setTimeout(updateIp, 1000 * 3600);
+  }
+
+  getGatewayId()
+  .then(function(storedId){
+    gatewayId = storedId.value;
+    return getToken();
+  })
+  .then(function(storedToken){
+    token = storedToken.value;
+    console.log('connecting to skynet...');
+    return subdevices.firstTime();
+  })
+  .then(function(){
+    return connectSkynet(gatewayId, token);
+  })
+  .then(function(skynetConnection){
+    conn = skynetConnection;
+    console.log('initializing subdevice instances...');
+    subdevicesMessenger = subdevices.initializeMessenger(conn, gatewayId);
+    return subdevices.initializeInstances(subdevicesMessenger);
+  })
+  .then(function(instances){
+    console.log('subdevice instances', instances);
+    console.log('starting webserver...');
+    webserver(gatewayId, token, port, conn);
+    console.log('starting ip updater...');
+    updateIp();
+  })
+  .otherwise(function(err){
+    console.log('error initializing this gateway', err);
+    process.exit(1);
+  });
+
+}
+
+module.exports = launch;
+
--- server.js
+++ server.js
@@ -1,52 +1,25 @@
-var db = require('./lib/level');
-var getToken = require('./lib/getToken');
-var webserver = require('./webserver');
-var sendLanIp = require('./lib/sendLanIp');
-var subdevices = require('./lib/subdevices');
-var getGatewayId = require('./lib/getGatewayId');
-var connectSkynet = require('./lib/connectSkynet');
+var cluster = require('cluster');
+var launch = require('./launcher');
 
+if (cluster.isMaster) {
+  cluster.fork();
 
+  cluster.on('exit', function(deadWorker, code, signal) {
+    console.log('exited code, signal', code, signal);
+    if(code == 42){
+      // Restart the worker
+      var worker = cluster.fork();
 
-var gatewayId;
-var token;
-var port = process.env.PORT || 8888;
-var conn;
-var subdevicesMessenger;
+      // Note the process IDs
+      var newPID = worker.process.pid;
+      var oldPID = deadWorker.process.pid;
 
-function updateIp(){
-  sendLanIp(gatewayId, token, port, conn);
-  setTimeout(updateIp, 1000 * 3600);
+      // Log the event
+      console.log('worker '+oldPID+' died.');
+      console.log('worker '+newPID+' born.');
+    }
+
+  });
+} else {
+  launch();
 }
-
-getGatewayId()
-.then(function(storedId){
-  gatewayId = storedId.value;
-  return getToken();
-})
-.then(function(storedToken){
-  token = storedToken.value;
-  console.log('connecting to skynet...');
-  return subdevices.firstTime();
-})
-.then(function(){
-  return connectSkynet(gatewayId, token);
-})
-.then(function(skynetConnection){
-  conn = skynetConnection;
-  console.log('initializing subdevice instances...');
-  subdevicesMessenger = subdevices.initializeMessenger(conn, gatewayId);
-  return subdevices.initializeInstances(subdevicesMessenger);
-})
-.then(function(instances){
-  console.log('subdevice instances', instances);
-  console.log('starting webserver...');
-  webserver(gatewayId, token, port, conn);
-  console.log('starting ip updater...');
-  updateIp();
-})
-.otherwise(function(err){
-  console.log('error initializing this gateway', err);
-  process.exit(1);
-});
-
