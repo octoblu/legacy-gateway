8775246b41f7a6e5abefb766e8f6e67825e5a8c7
Luis Montes <luis@iceddev.com>
2014-07-06 21:10:55 -0700

    refactor launcher

--- launcher.js
+++ launcher.js
@@ -1,7 +1,6 @@
 var when = require('when');
 var getToken = require('./lib/getToken');
 var webserver = require('./webserver');
-var sendLanIp = require('./lib/sendLanIp');
 var subdevices = require('./lib/subdevices');
 var getGatewayId = require('./lib/getGatewayId');
 var connectSkynet = require('./lib/connectSkynet');
@@ -9,29 +8,18 @@ var connectSkynet = require('./lib/connectSkynet');
 
 function launch(){
 
-
-  var gatewayIdPromise = getGatewayId();
-  var tokenPromise = getToken();
-  var firstTimePromise = subdevices.firstTime();
-
-  var connectionPromise = when.join(gatewayIdPromise, tokenPromise, firstTimePromise)
+    when.join(getGatewayId(), getToken(), subdevices.firstTime())
     .spread(connectSkynet)
-
     .then(function(skynetConnection){
-      // updateIp();
       console.log('initializing subdevice instances...');
       var subdevicesMessenger = subdevices.initializeMessenger(skynetConnection);
       return subdevices.initializeInstances(subdevicesMessenger).yield(skynetConnection);
     })
-    .then()
+    .then(webserver)
     .catch(function(err){
       console.log('error initializing this gateway', err);
       process.exit(1);
     });
-
-  when.join(gatewayIdPromise, tokenPromise, connectionPromise)
-    .spread(webserver);
-
 
 }
 
